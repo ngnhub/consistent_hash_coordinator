a4642a60b177d5c521033038c3dfed5184423082
ngnhub <nagovicynalexandr@gmail.com>
2024-11-10 18:51:26 +0100

    removed hashed interface

--- consistent_hash/src/main/kotlin/com/github/ngnhub/consistent_hash/model/Hashed.kt
+++
@@ -1,7 +0,0 @@
-package com.github.ngnhub.consistent_hash.model
-
-import java.math.BigInteger
-
-interface Hashed {
-    val hash: BigInteger
-}
--- coordinator/src/main/kotlin/com/github/ngnhub/partition_coordinator/Server.kt
+++ coordinator/src/main/kotlin/com/github/ngnhub/partition_coordinator/Server.kt
@@ -1,11 +1,6 @@
 package com.github.ngnhub.partition_coordinator
 
-import com.github.ngnhub.consistent_hash.model.Hashed
-import java.math.BigInteger
-
-interface Server<K> : Hashed {
-
-    override val hash: BigInteger
+interface Server<K> {
 
     val key: String // todo shouldn't be here. it is a ConsistentHash's responsibility
 
