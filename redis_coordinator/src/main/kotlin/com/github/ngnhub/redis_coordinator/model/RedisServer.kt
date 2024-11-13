dd824b178d5ec6c4c027c4056baed4a2ca305fc4
ngnhub <nagovicynalexandr@gmail.com>
2024-11-13 21:07:41 +0100

    default redis redistribution impl

--- redis_coordinator/src/main/kotlin/com/github/ngnhub/redis_coordinator/model/RedisServer.kt
+++ redis_coordinator/src/main/kotlin/com/github/ngnhub/redis_coordinator/model/RedisServer.kt
@@ -1,27 +1,36 @@
 package com.github.ngnhub.redis_coordinator.model
 
+import com.github.ngnhub.consistent_hash.HashFunction
 import com.github.ngnhub.partition_coordinator.Server
+import redis.clients.jedis.Jedis
 import redis.clients.jedis.JedisPool
 import redis.clients.jedis.JedisPoolConfig
+import redis.clients.jedis.params.MigrateParams
+import redis.clients.jedis.params.ScanParams
 
 class RedisServer(
-    override val host: String,
-    override val port: Int,
-    override val virtualNodesKeys: Set<String> = mutableSetOf(),
-) : Server {
+    host: String,
+    port: Int,
+    private val redistributePageSize: Int,
+    private val redisPool: JedisPool = JedisPool(JedisPoolConfig(), host, port) // todo async lib
+) : Server(host, port) {
 
-    private val redisTemplate = JedisPool(JedisPoolConfig(), host, port);
-
-    override fun insert(key: String, value: Any?) {
-        redisTemplate.resource.use { redis -> redis.set(key, value.toString()) }
-        value?.let { }
+    override fun reDistribute(from: Server, by: HashFunction<String>) {
+        val count = ScanParams().count(redistributePageSize)
+        val redisServer = from as RedisServer
+        redisServer.redisPool.resource.use { resource -> migrateBatched(by, resource, count) }
     }
 
-    override fun reDistribute(from: Server) {
-        TODO("Not yet implemented")
-    }
-
-    override fun read(key: String): Any? {
-        return redisTemplate.resource.use { redis -> redis.get(key) }
+    private fun migrateBatched(by: HashFunction<String>, resource: Jedis, count: ScanParams?) {
+        var scan = resource.scan("0")
+        while (scan != null) {
+            val keysForMigration = scan.result
+                .filter { key -> by.hash(key) <= hash }
+                .toTypedArray()
+            val migrateParams = MigrateParams().copy()
+            val timeout = 3000 // todo: how to chose? how to handle if timed out
+            resource.migrate(host, port, timeout, migrateParams, *keysForMigration) // todo what if key is already removed
+            scan = resource.scan(scan.cursor, count)
+        }
     }
 }
